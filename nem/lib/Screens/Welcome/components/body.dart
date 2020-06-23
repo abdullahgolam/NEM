dcfe2414631ebbb3a9f79c51c1af2f5623010d4e
Abdullah A. Golam <abdullahagolam@gmail.com>
2020-06-24 01:36:26 +0300

    welcome page

---
+++ nem/lib/Screens/Welcome/components/body.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:nem/Screens/Login/login_screen.dart';
+import 'package:nem/components/rounded_button.dart';
+import 'package:nem/constants.dart';
+import 'background.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Welcome to NEOM Energy Management",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.05),
+            Image.asset(
+              "assets/icons/HngcBzxf_400x400.jpg",
+              height: size.height * 0.45,
+            ),
+            SizedBox(height: size.height * 0.05),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            RoundedButton(
+              text: "SIGNUP",
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+              press: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ nem/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+
+// const kPrimaryColor = Color(0xFF008B8B);
+// const kPrimaryLightColor = Color(0xFFE0FFFF);
