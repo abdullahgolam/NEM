85f650a4de491af3ff1b785cd4962d8228e8786b
Abdullah A. Golam <abdullahagolam@gmail.com>
2020-07-14 21:56:31 +0300

    Home statictic page

--- nem/lib/Screens/HomeStatistics/home_statistics.dart
+++ nem/lib/Screens/HomeStatistics/home_statistics.dart
@@ -10,11 +10,65 @@ class _HomeStatisticsState extends State<HomeStatistics> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: kPrimaryColor,
           title: Text("Home Statistics"),
+        ),
+        body: Container(
+          width: double.infinity,
+          height: size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Image.asset(
+                "assets/icons/icons8-bill-80.png",
+                height: size.height * 0.20,
+              ),
+              Text(
+                "Current Bill ---- SR",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Text(
+                "(18 days)",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Image.asset(
+                "assets/icons/icons8-energy-meter-80.png",
+                height: size.height * 0.20,
+              ),
+              Text(
+                "Going-On Consumption ",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Text(
+                "---- per/hr",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(width: 1),
+                  borderRadius: BorderRadius.circular(28.0),
+                ),
+                margin: const EdgeInsets.all(30),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 30),
+                  child: Text(
+                    'Details of Manufacturers and Appliances is available here',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 18, height: 1.5),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
         drawer: Drawer(
           // Add a ListView to the drawer. This ensures the user can scroll
